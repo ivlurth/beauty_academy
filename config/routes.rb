6bbe0f884dd9990ee1d2154dc6900a658aca5dab
ivlurth <eturlovich@gmail.com>
2023-01-09 03:59:24 +0300

    Devise added to model users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
