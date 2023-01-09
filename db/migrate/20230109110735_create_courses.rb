c052290cdb2c0278f20e3968f8ffeaf2d7e93f00
ivlurth <eturlovich@gmail.com>
2023-01-09 14:41:34 +0300

    Added Course model and controller

---
+++ db/migrate/20230109110735_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.text :description_short
+      t.text :description_long
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_021809) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_110735) do
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description_short"
+    t.text "description_long"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
