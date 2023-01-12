8e732180e3ba65af33c777fd20eb96bdb1b940b6
ivlurth <eturlovich@gmail.com>
2023-01-12 05:45:30 +0300

    Added user profile configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:phone)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :courses
   resources :users
+  resources :owned_courses
+
   # Defines the root path route ("/")
   root "welcome#index"
 end
